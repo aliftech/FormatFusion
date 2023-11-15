19bdbc82b5427c4df64b5b7fb339ebe6cab8e07b
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-11-15 17:04:48 +0700

    ci(setup.py,-setup.cfg): add configuration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='YAMLify',
+    packages=['YAMLify'],
+    version='1.0.0',  # Ideally should be same as your github release tag varsion
+    description='YAMLify is a versatile and user-friendly tool that facilitates seamless conversion between YAML and JSON data formats. It offers a comprehensive set of features that cater to diverse needs, making it an invaluable asset for developers, data analysts, and anyone working with these data formats.',
+    author='Wahyu Krisna Aji',
+    author_email='wahyukrisnaaji32@gmail.com',
+    url='https://github.com/aliftech/YAMLify',
+    download_url='https://github.com/aliftech/YAMLify',
+    keywords=['yaml-to-json', 'json-to-yaml', 'python'],
+    classifiers=[],
+)
