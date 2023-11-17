69583fe0849c6cc1794c8cf70737a2393a05f614
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-11-17 10:56:37 +0700

    build: build stable version 1.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yamlify",
-    version="1.2.1",
+    version="1.2.3",
     description="YAMLify is a versatile and user-friendly tool that facilitates seamless conversion between YAML and JSON data formats. It offers a comprehensive set of features that cater to diverse needs, making it an invaluable asset for developers, data analysts, and anyone working with these data formats.",
     long_description=open("README.md").read() + "\n\n" +
     open("CHANGELOG.txt").read(),
@@ -11,11 +11,9 @@ setup(
     author_email="wahyukrisnaaji32@gmail.com",
     license="MIT",
     classifiers=[
-        "Development Status :: 5 — Production/Stable",
-        "Intended Audience :: Education",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
     ],
-    keywords="yamlify",
+    keywords=["yamlify", "json_to_yaml", "yaml_to_json"],
 )
--- yamlify/__init__.py
+++ yamlify/__init__.py
@@ -1,2 +1,2 @@
-from yaml_to_json import *
-from json_to_yaml import *
+from .json_to_yaml import json_to_yaml
+from .yaml_to_json import yaml_to_json, yaml_to_json_from_file, yaml_to_json_from_url, yaml_to_json_to_file, yaml_to_json_to_url
