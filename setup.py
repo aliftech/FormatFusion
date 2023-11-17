0022b19e29f157af93577fdf2aa218debb698c17
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-11-17 09:52:35 +0700

    build(dist,-pyproject.toml,-setup.py,-yamlify,-yamlify.egg-info): build stable version yamlify v.1.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="yamlify",
-    version="1.1.0",
+    version="1.1.1",
     description="YAMLify is a versatile and user-friendly tool that facilitates seamless conversion between YAML and JSON data formats. It offers a comprehensive set of features that cater to diverse needs, making it an invaluable asset for developers, data analysts, and anyone working with these data formats.",
     long_description=open("README.md").read() + "\n\n" +
     open("CHANGELOG.txt").read(),
--- test/test.py
+++ test/test.py
@@ -1,6 +1,5 @@
 import unittest
-from yamlify.yaml_to_json import *
-from yamlify.json_to_yaml import *
+import yamlify as yf
 
 
 class YAMLToJSONTests(unittest.TestCase):
@@ -14,28 +13,28 @@ class YAMLToJSONTests(unittest.TestCase):
             city: Jakarta
             province: DKI Jakarta
         """
-        json_data = yaml_to_json(yaml_data)
+        json_data = yf.yaml_to_json(yaml_data)
         return json_data
 
     def test_yaml_to_json_with_indent(self):
         yaml_data = "name: John Doe\nage: 30\ncity: Seattle"
-        json_data = yaml_to_json(yaml_data, indent=2)
+        json_data = yf.yaml_to_json(yaml_data, indent=2)
         return json_data
 
     def test_yaml_to_json_with_sort_keys(self):
         yaml_data = "city: Seattle\nage: 30\nname: John Doe"
-        json_data = yaml_to_json(yaml_data, sort_keys=True)
+        json_data = yf.yaml_to_json(yaml_data, sort_keys=True)
         return json_data
 
     def test_yaml_to_json_from_file(self):
         with open('test.yaml', 'r') as f:
             yaml_data = f.read()
-        actual_json = yaml_to_json(yaml_data)
+        actual_json = yf.yaml_to_json(yaml_data)
         return actual_json
 
     def test_yaml_to_json_from_url(self):
         url = 'https://github.com/aliftech/YAMLify/blob/master/test.yaml'
-        json_data = yaml_to_json_from_url(url)
+        json_data = yf.yaml_to_json_from_url(url)
         return json_data
 
 
@@ -50,7 +49,7 @@ class JSONToYAMLTests(unittest.TestCase):
         }
         '''
 
-        yaml_data = json_to_yaml(json_data)
+        yaml_data = yf.json_to_yaml(json_data)
         return yaml_data
 
 
