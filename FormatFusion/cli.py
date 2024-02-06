09a884ac572edb67613c29ec1ee6fe0c9fd201f1
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2024-02-06 12:07:19 +0700

    feat(FormatFusion/cli.py): add command line interface

---
+++ FormatFusion/cli.py
@@ -0,0 +1,117 @@
+import os
+import reader
+import csv_to_json
+import csv_to_xml
+import csv_to_yaml
+import json_to_csv
+import json_to_xml
+import json_to_yaml
+import xml_to_csv
+import xml_to_json
+import xml_to_yaml
+import yaml_to_csv
+import yaml_to_json
+import yaml_to_xml
+
+
+import os
+import argparse
+
+
+def convert_file(input_file, output_file):
+    """
+    Mengubah file dari format input ke format output.
+
+    Args:
+        input_file (str): Jalur file input.
+        output_file (str): Jalur file output.
+
+    Returns:
+        None.
+    """
+
+    if not os.path.exists(input_file):
+        print(f"File input '{input_file}' not found.")
+        return
+
+    content = reader.read_file(input_file)
+    if content is False:
+        return "Unsupported file input format."
+
+    input_format = reader.validate_format(input_file)
+    if input_format is False:
+        return "Unsupported file input format."
+    else:
+        output_format = reader.validate_format(output_file)
+        if output_format is False:
+            return "Unsupported file output format."
+        else:
+            if input_format == "csv" and output_format == "json":
+                converted_data = csv_to_json.csv_to_json(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "csv" and output_format == "xml":
+                converted_data = csv_to_xml.csv_to_xml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "csv" and output_format == "yaml":
+                converted_data = csv_to_yaml.csv_to_yaml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "json" and output_format == "csv":
+                converted_data = json_to_csv.json_to_csv(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "json" and output_format == "xml":
+                converted_data = json_to_xml.json_to_xml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "json" and output_format == "yaml":
+                converted_data = json_to_yaml.json_to_yaml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "xml" and output_format == "csv":
+                converted_data = xml_to_csv.xml_to_csv(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "xml" and output_format == "json":
+                converted_data = xml_to_json.xml_to_json(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "xml" and output_format == "yaml":
+                converted_data = xml_to_yaml.xml_to_yaml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "yaml" and output_format == "csv":
+                converted_data = yaml_to_csv.yaml_to_csv(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "yaml" and output_format == "json":
+                converted_data = yaml_to_json.yaml_to_json(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            elif input_format == "yaml" and output_format == "xml":
+                converted_data = yaml_to_xml.yaml_to_xml(content)
+                reader.save_file(output_file, converted_data)
+                return f"Converting {input_file} into {output_file} is success"
+            else:
+                return f"The {input_file} format or {output_file} format is not supported."
+
+
+def main():
+    """
+    Fungsi utama CLI.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Convert files to another file format.")
+    parser.add_argument("input_file", help="path input file.")
+    parser.add_argument("output_file", help="path output file.")
+
+    args = parser.parse_args()
+
+    convert_file(args.input_file, args.output_file)
+
+
+if __name__ == "__main__":
+    main()
--- FormatFusion/reader.py
+++ FormatFusion/reader.py
@@ -3,9 +3,12 @@ import os
 
 
 def read_file(filename):
-    with open(filename, 'r') as file:
-        result = file.read()
-    return result
+    if validate_format(filename) is not False:
+        with open(filename, 'r') as file:
+            result = file.read()
+        return result
+    else:
+        return False
 
 
 def scan_url(url):
@@ -19,3 +22,33 @@ def scan_url(url):
 def save_file(filename, data):
     with open(filename, 'w') as file:
         file.write(str(data))
+
+
+def validate_format(filename):
+    """
+    Memeriksa apakah format file yang diberikan cocok dengan format yang didukung dan mengembalikan formatnya jika valid, False jika tidak.
+
+    Args:
+        filename (str): Nama file yang akan divalidasi.
+
+    Returns:
+        str: Format file (".json", ".yaml", dll.) jika valid, False jika tidak.
+    """
+
+    supported_formats = {
+        "JSON": ".json",
+        "YAML": ".yaml",
+        "XML": ".xml",
+        "CSV": ".csv",
+        # Tambahkan format lainnya di sini, pastikan konsistensi casing dengan key yang sudah ada
+    }
+
+    _, extension = os.path.splitext(filename.lower())
+
+    if extension in supported_formats.values():
+        # Case-insensitive match: Temukan key yang cocok berdasarkan value
+        format_name = max(format_name for format_name,
+                          format_ext in supported_formats.items() if format_ext == extension)
+        return format_name.lower()  # Kembalikan format string yang sesuai
+    else:
+        return False
